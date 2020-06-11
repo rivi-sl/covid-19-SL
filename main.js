f508453b5524beb5950de38292ad8ded3d5f58fc
chamodperera <chamodperera87@gmail.com>
2020-06-11 14:31:25 +0530

    added database

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function add_data(doc){
   let count = document.createElement('div');
 
 
-  li.setAttribute('data-id',doc.id);
+  li.setAttribute('data-id',doc.data);
   name.textContent = doc.data().a_name;
   count.textContent = doc.data().b_count;
 
@@ -23,3 +23,70 @@ db.collection('Data').get().then((snapshot) => {     //grab the collections and 
         add_data(doc); //gets data
     })
 });
+
+//added precentages
+const precentag1 = document.querySelector('#par1');
+const precentag2 = document.querySelector('#par2');
+
+var doc1 = db.collection("Data").doc("a_totalConfirmed");
+doc1.get().then(function par(doc)  {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+        var confirmed = doc.data().b_count;
+    } 
+
+var doc2 = db.collection("Data").doc("b_deaths");
+doc2.get().then(function par(doc)  {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+         var deaths = doc.data().b_count;
+    }
+
+    let li = document.createElement('li'); 
+    let name = document.createElement('span');
+    let count = document.createElement('div');
+    
+    li.setAttribute('data-id',doc.data);
+    name.textContent = "Death Rate";
+    var value =deaths/confirmed*100;
+    count.textContent = value.toFixed(2)+"%";
+
+  
+    li.appendChild(name);
+    li.appendChild(count);
+    
+   precentag1.appendChild(li);
+
+    
+var doc3 = db.collection("Data").doc("c_totalRecovered");
+doc3.get().then(function par(doc)  {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+        var recover = doc.data().b_count;
+    } 
+
+    let li = document.createElement('li'); 
+    let name = document.createElement('span');
+    let count = document.createElement('div');
+    
+    li.setAttribute('data-id',doc.data);
+    name.textContent = "Recovery Rate";
+    var value =recover/confirmed*100;
+    count.textContent = value.toFixed(2)+"%";
+
+  
+    li.appendChild(name);
+    li.appendChild(count);
+    
+   precentag2.appendChild(li);
+
+});
+});
+});
+
+
+
+
+
+
+
